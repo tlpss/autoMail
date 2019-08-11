0807bcd0a01dabb4d1c0da4dae8b60bb44f4699f
tlips <thomas.lips@ugent.be>
2019-08-11 11:57:56 +0200

    documentation for config file

--- configuration.py
+++ configuration.py
@@ -1,3 +1,8 @@
 class Configuration:
+    """
+    user = Gmail account (i.e. primary gmail mail address)
+    app_pwd is an application password created with your google account
+        https://support.google.com/accounts/answer/185833?hl=en
+    """
     user = "<fill>"
     app_pwd  ="<fill>"
